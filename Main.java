56ad5e392b9919dcfafc3534318152787dd7d273
Karthik Vemparala <76411921+karthik-vemp@users.noreply.github.com>
2023-05-21 10:22:15 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+/*
+* Sunday May 21, 2023
+* @Authors: Elisia Rosel, Jocelyn Rosel, Bhavya Veerapaneni, Karthik Vemperala
+*
+* Description: This webapp aim is to help those facing artists block. Using our vast database, the AllCrafts class filters information to provide the user with crafts they might be interested in. The user can input keywords, their difficulty level, and categories they're interested in and the program will generate results for them. 
+*
+*/
+public class Main {
+    public static void main(String[] args) {
+        AllCrafts c = new AllCrafts();
+        c.run();
+    }
+}
